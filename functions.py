398d65eb3ea189c38447ddb1d4fe85d8f18cb8d4
Jasmine8596 <jasmine.padhye.0805@gmail.com>
2018-12-02 19:04:17 +0100

    detecting number of objects

--- functions.py
+++ functions.py
@@ -2,12 +2,24 @@
 
 class combine_modalities:
     def __init__ (self):
-        return None
+        self.number_of_sensors = 0
+        self.number_of_objects = 0
 
     def count_number_of_sensors(self, sensor_input):
-        number_of_sensors = len(sensor_input)
+        self.number_of_sensors = len(sensor_input)
 
-        return number_of_sensors
+        return self.number_of_sensors
+
+    def detect_number_of_objects(self,sensor_inputs):
+
+        for number in range(self.number_of_sensors):
+            reading = sensor_inputs[number]
+            number_of_reading = len(reading)
+
+            if number_of_reading > self.number_of_objects:
+                self.number_of_objects = number_of_reading
+
+        return self.number_of_objects
 
 
 if __name__ == "__main__":
@@ -18,3 +30,6 @@ if __name__ == "__main__":
 
     result = obj.count_number_of_sensors([sensor1,sensor2])
     print("Number of sensors:",result)
+
+    result = obj.detect_number_of_objects([sensor1,sensor2])
+    print("Number of objects detected:",result)
