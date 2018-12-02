dc64c7fc7ac47dd91f663615ec221291fee6b029
Jasmine8596 <jasmine.padhye.0805@gmail.com>
2018-12-02 19:28:07 +0100

    detected objects,test passed

--- functions.py
+++ functions.py
@@ -4,6 +4,7 @@ class combine_modalities:
     def __init__ (self):
         self.number_of_sensors = 0
         self.number_of_objects = 0
+        self.objects_detected = []
 
     def count_number_of_sensors(self, sensor_input):
         self.number_of_sensors = len(sensor_input)
@@ -21,15 +22,33 @@ class combine_modalities:
 
         return self.number_of_objects
 
+    def detect_objects(self,sensor_inputs):
+
+        for sensor in sensor_inputs:
+            for object in sensor:
+                name = object[0]
+
+                if name not in self.objects_detected:
+                    self.objects_detected.append(name)
+                else:
+                    self.objects_detected = []
+                    break
+
+        return self.objects_detected
+
 
 if __name__ == "__main__":
     obj = combine_modalities()
 
     sensor1 = [('knife', 1, 99), ('scissor', 2, 65), ('spoon', 3, 33), ('spoon', 4, 80), ('keys', 5, 95)]
     sensor2 = [('knife', 1, 55), ('scissor', 2, 95), ('fork', 3, 99), ('spoon', 4, 99), ('keys', 5, 95)]
+    sensor_input = [sensor1,sensor2]
 
-    result = obj.count_number_of_sensors([sensor1,sensor2])
+    result = obj.count_number_of_sensors(sensor_input)
     print("Number of sensors:",result)
 
-    result = obj.detect_number_of_objects([sensor1,sensor2])
+    result = obj.detect_number_of_objects(sensor_input)
     print("Number of objects detected:",result)
+
+    result = obj.detect_objects(sensor_input)
+    print("Objects detected:",result)
