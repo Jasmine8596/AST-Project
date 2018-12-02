6a32b8dc365e89724ba45e9df6b83258d520f5cc
21akashuh <akashu.hulihalli@gmail.com>
2018-12-02 20:14:45 +0100

    modifying test cases

--- test_functions.py
+++ test_functions.py
@@ -7,8 +7,9 @@ class TestCodes(unittest.TestCase):
     test = combine_modalities()
     sensor1 = [('knife', 1, 99), ('scissor', 2, 65), ('spoon', 3, 33), ('spoon', 4, 80), ('keys', 5, 95)]
     sensor2 = [('knife', 1, 55), ('scissor', 2, 95), ('fork', 3, 99), ('spoon', 4, 99), ('keys', 5, 95)]
-    objects = ["knife", "scissor", "fork", "spoon", "keys"]
+    objects_detected = ["knife", "scissor", "fork", "spoon", "keys"]
     percentage_required = [99, 95, 99, 99, 95]
+    objects = []
 
     def test_count_number_of_sensors(self):
 
@@ -23,22 +24,22 @@ class TestCodes(unittest.TestCase):
     def test_detect_objects(self):
 
         result = TestCodes.test.detect_objects([TestCodes.sensor1,TestCodes.sensor2])
-        self.assertEqual(TestCodes.objects,result)
+        self.assertEqual(TestCodes.objects_detected,result)
 
     def test_create_objects(self):
 
-        result = TestCodes.test.create_objects(TestCodes.objects, [TestCodes.sensor1, TestCodes.sensor2])
-        self.assertEqual(TestCodes.objects[0], result[0].name)
-        self.assertEqual(TestCodes.objects[1], result[1].name)
-        self.assertEqual(TestCodes.objects[2], result[2].name)
-        self.assertEqual(TestCodes.objects[3], result[3].name)
-        self.assertEqual(TestCodes.objects[4], result[4].name)
+        TestCodes.objects = TestCodes.test.create_objects(TestCodes.objects_detected, [TestCodes.sensor1, TestCodes.sensor2])
+        self.assertEqual(TestCodes.objects_detected[0], TestCodes.objects[0].name)
+        self.assertEqual(TestCodes.objects_detected[1], TestCodes.objects[1].name)
+        self.assertEqual(TestCodes.objects_detected[2], TestCodes.objects[2].name)
+        self.assertEqual(TestCodes.objects_detected[3], TestCodes.objects[3].name)
+        self.assertEqual(TestCodes.objects_detected[4], TestCodes.objects[4].name)
 
-        self.assertEqual(TestCodes.percentage_required[0], int(result[0].percentage))
-        self.assertEqual(TestCodes.percentage_required[1], int(result[1].percentage))
-        self.assertEqual(TestCodes.percentage_required[2], int(result[2].percentage))
-        self.assertEqual(TestCodes.percentage_required[3], int(result[3].percentage))
-        self.assertEqual(TestCodes.percentage_required[4], int(result[4].percentage))
+        self.assertEqual(TestCodes.percentage_required[0], int(TestCodes.objects[0].percentage))
+        self.assertEqual(TestCodes.percentage_required[1], int(TestCodes.objects[1].percentage))
+        self.assertEqual(TestCodes.percentage_required[2], int(TestCodes.objects[2].percentage))
+        self.assertEqual(TestCodes.percentage_required[3], int(TestCodes.objects[3].percentage))
+        self.assertEqual(TestCodes.percentage_required[4], int(TestCodes.objects[4].percentage))
 
 
 if __name__ == "__main__":
