8bf95571abcdb491efc7964b9c8375f7cef69735
Jasmine8596 <jasmine.padhye.0805@gmail.com>
2018-12-02 19:38:17 +0100

    adding class of objects

---
+++ objects.py
@@ -0,0 +1,7 @@
+
+class Object:
+
+    def __init__(self,object_name=None,object_num=0,object_percentage=0):
+        self.name = object_name
+        self.number = object_num
+        self.percentage = object_percentage
