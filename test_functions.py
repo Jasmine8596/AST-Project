e308df82e948c83c73649a5a136980a4ce6aa428
21akashuh <akashu.hulihalli@gmail.com>
2018-12-02 18:49:06 +0100

    adding second test case

--- test_functions.py
+++ test_functions.py
@@ -13,5 +13,15 @@ class TestCodes(unittest.TestCase):
         result = test.count_number_of_sensors([sensor1,sensor2])
         self.assertEqual(2,result)
 
+    def test_detect_number_of_objects(self):
+        test = combine_modalities()
+
+        sensor1 = [('knife', 1, 99), ('scissor', 2, 65), ('spoon', 3, 33), ('spoon', 4, 80), ('keys', 5, 95)]
+        sensor2 = [('knife', 1, 55), ('scissor', 2, 95), ('fork', 3, 99), ('spoon', 4, 99), ('keys', 5, 95)]
+
+        result = test.detect_number_of_objects([sensor1,sensor2])
+        self.assertEqual(5,result)
+
+
 if __name__ == "__main__":
     unittest.main(exit=False)
