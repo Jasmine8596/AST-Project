9d9556841d998725921abb36ce05382ec494cec8
21akashuh <akashu.hulihalli@gmail.com>
2018-12-02 19:14:13 +0100

    adding detect objects test case

--- test_functions.py
+++ test_functions.py
@@ -7,6 +7,7 @@ class TestCodes(unittest.TestCase):
     test = combine_modalities()
     sensor1 = [('knife', 1, 99), ('scissor', 2, 65), ('spoon', 3, 33), ('spoon', 4, 80), ('keys', 5, 95)]
     sensor2 = [('knife', 1, 55), ('scissor', 2, 95), ('fork', 3, 99), ('spoon', 4, 99), ('keys', 5, 95)]
+    objects = ["knife", "scissor", "fork", "spoon", "keys"]
 
     def test_count_number_of_sensors(self):
 
@@ -17,6 +18,11 @@ class TestCodes(unittest.TestCase):
 
         result = TestCodes.test.detect_number_of_objects([TestCodes.sensor1,TestCodes.sensor2])
         self.assertEqual(5,result)
+
+    def test_detect_objects(self):
+
+        result = TestCodes.test.detect_objects([TestCodes.sensor1,TestCodes.sensor2])
+        self.assertEqual(TestCodes.objects,result)
 
 
 if __name__ == "__main__":
