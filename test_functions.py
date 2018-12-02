5ed959d74cb8094ef34d6b02822d6955348e30db
21akashuh <akashu.hulihalli@gmail.com>
2018-12-02 19:09:16 +0100

    small changes in test cases

--- test_functions.py
+++ test_functions.py
@@ -4,22 +4,18 @@ import unittest
 
 class TestCodes(unittest.TestCase):
 
+    test = combine_modalities()
+    sensor1 = [('knife', 1, 99), ('scissor', 2, 65), ('spoon', 3, 33), ('spoon', 4, 80), ('keys', 5, 95)]
+    sensor2 = [('knife', 1, 55), ('scissor', 2, 95), ('fork', 3, 99), ('spoon', 4, 99), ('keys', 5, 95)]
+
     def test_count_number_of_sensors(self):
-        test = combine_modalities()
 
-        sensor1 = [('knife', 1, 99), ('scissor', 2, 65), ('spoon', 3, 33), ('spoon', 4, 80), ('keys', 5, 95)]
-        sensor2 = [('knife', 1, 55), ('scissor', 2, 95), ('fork', 3, 99), ('spoon', 4, 99), ('keys', 5, 95)]
-
-        result = test.count_number_of_sensors([sensor1,sensor2])
+        result = TestCodes.test.count_number_of_sensors([TestCodes.sensor1,TestCodes.sensor2])
         self.assertEqual(2,result)
 
     def test_detect_number_of_objects(self):
-        test = combine_modalities()
 
-        sensor1 = [('knife', 1, 99), ('scissor', 2, 65), ('spoon', 3, 33), ('spoon', 4, 80), ('keys', 5, 95)]
-        sensor2 = [('knife', 1, 55), ('scissor', 2, 95), ('fork', 3, 99), ('spoon', 4, 99), ('keys', 5, 95)]
-
-        result = test.detect_number_of_objects([sensor1,sensor2])
+        result = TestCodes.test.detect_number_of_objects([TestCodes.sensor1,TestCodes.sensor2])
         self.assertEqual(5,result)
 
 
