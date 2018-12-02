a9fe6957590e8b3f901bc0190e811b9ace4005e8
21akashuh <akashu.hulihalli@gmail.com>
2018-12-02 18:17:10 +0100

    Adding structure of codes

---
+++ functions.py
@@ -0,0 +1,9 @@
+
+
+class combine_modalities:
+    def __init__ (self):
+        return None
+
+
+if __name__ == "__main__":
+    obj = combine_modalities()
---
+++ test_functions.py
@@ -0,0 +1,10 @@
+from functions import combine_modalities
+import unittest
+
+
+class TestCodes(unittest.TestCase):
+    def __init__(self):
+        return None
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
