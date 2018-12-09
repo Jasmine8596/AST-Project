c301755d88685601e81d33ef659a409b0c914110
Jasmine8596 <jasmine.padhye.0805@gmail.com>
2018-12-09 12:38:14 +0100

    removed redundant code

--- functions.py
+++ functions.py
@@ -20,43 +20,8 @@ class CombineModalities:
 
     def detect_number_of_objects(self,sensor_inputs):
 
-        for number in range(self.number_of_sensors):
-            reading = sensor_inputs[number]
-            number_of_reading = len(reading)
-
-            if number_of_reading > self.number_of_objects:
-                self.number_of_objects = number_of_reading
-
         return self.number_of_objects
 
-    def detect_objects(self,sensor_inputs):
-
-        for sensor in sensor_inputs:
-            for object in sensor:
-                name = object[0]
-
-                if name not in self.objects_detected:
-                    self.objects_detected.append(name)
-                else:
-                    self.objects_detected = []
-                    break
-
-        return self.objects_detected
-
-    def create_objects(self, sensor_inputs, sensors):
-
-        for index, object_found in enumerate(sensor_inputs):
-            created_object = Object(object_found, index + 1)
-            self.objects.append(created_object)
-
-        sensors = np.array(sensors)
-        percentages = sensors[:, :, 2]
-        percentages = percentages.T
-
-        for index, percentage in enumerate(percentages):
-            self.objects[index].percentage = max(percentage)
-
-        return self.objects
 
 
 if __name__ == "__main__":
