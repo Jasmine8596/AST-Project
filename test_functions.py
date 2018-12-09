bdb9ae12f535611f96bf29d248b039d0afa83d28
21akashuh <akashu.hulihalli@gmail.com>
2018-12-09 12:48:14 +0100

    test case added

--- test_functions.py
+++ test_functions.py
@@ -12,18 +12,21 @@ class TestCodes(unittest.TestCase):
     sensor2 = InputSensor()
     sensor2.data = [("keys", 5, 95), ("spoon", 4, 99), ("fork", 3, 99), ("scissor", 2, 95), ("knife", 1, 55)]
 
+    sensor_input = [sensor1,sensor2]
+
     objects_detected = []
     percentage_required = []
     objects = []
 
     def test_count_number_of_sensors(self):
 
-        result = TestCodes.test.count_number_of_sensors([TestCodes.sensor1,TestCodes.sensor2])
+        result = TestCodes.test.count_number_of_sensors(TestCodes.sensor_input)
         self.assertEqual(2,result)
 
     def test_detect_number_of_objects(self):
 
-        pass
+        number_of_objects_actual = TestCodes.test.detect_number_of_objects(TestCodes.sensor_input)
+        self.assertEqual(5,number_of_objects_actual)
 
     def test_detect_objects(self):
 
